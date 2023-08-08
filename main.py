7e2a86fd8644b3f165c0c9a4518147ee8f1f3f33
happeii <akahappeii@gmail.com>
2023-08-08 16:32:22 -0400

    added enemies to the game

--- main.py
+++ main.py
@@ -22,7 +22,9 @@ timer = pygame.time.Clock()
 #images
 background = pygame.transform.scale(pygame.image.load("assets/bg.png"),(width,height))
 player = pygame.transform.scale(pygame.image.load("assets/harpoon boat.png"),(90,30))
-harpoon = pygame.transform.scale(pygame.image.load("assets/harpoon.png"),(90,30))
+harpoon = pygame.transform.scale(pygame.image.load("assets/harpoon.png"),(60,20))
+fish = pygame.transform.scale(pygame.image.load("assets/8-bit fish.png"),(70,70))
+fish_bullet = pygame.transform.scale(pygame.image.load("assets/temp fish bullet.png"),(50,10))
 
 
 
@@ -47,21 +49,43 @@ class Player(character):
         self.mask = pygame.mask.from_surface(self.image)
         self.max_hp = hp
 
+class Enemy(character):
+    UNITS = {
+        "atkfish": (fish, fish_bullet)
+     }
+    def __init__(self, x, y, unit, hp=100):
+        super().__init__(x, y, hp)
+        self.image,self.shot = self.UNITS[unit]
+        self.mask = pygame.mask.from_surface(self.image)
+
+    def move(self, speed):
+        self.x -= speed
+
 def main_loop():
     running = True
     score = 0
+    wave = 0
     lives = 3
     main_text = pygame.font.SysFont('freesansbold',40)
     player_speed = 5
-    player = Player(100,600)
+    player = Player(100,400)
+    enemies = []
+    enemy_speed = 2
+    wave_size = 5
 
     def update_screen():
         screen.blit(background,(0,0))
         score_text=main_text.render(f'SCORE: {score}',True,(255,255,255))
+        wave_text=main_text.render(f'WAVE: {wave}',True,(255,255,255))
         lives_text=main_text.render(f'LIVES: {lives}',True,(255,255,255))
         screen.blit(score_text,(10,10))
         screen.blit(lives_text,(800,10))
+        screen.blit(wave_text,(10,40))
         player.draw(screen)
+        for enemy in enemies:
+            enemy.draw(screen)
+
+
         pygame.display.update
 
 
@@ -69,6 +93,16 @@ def main_loop():
         timer.tick(fps)
         update_screen()
         
+        #waves
+        if len(enemies) == 0:
+            wave += 1
+            wave_size += 5
+            for i in range (wave_size):
+                enemy = Enemy(random.randrange(width + 100, width + 1000),
+                            random.randrange(100, height - 100),
+                            random.choice(["atkfish"]))
+                enemies.append(enemy)
+
         #movement
         active_keys = pygame.key.get_pressed()
         if active_keys[pygame.K_a] and player.x - player_speed > 0:
@@ -80,7 +114,8 @@ def main_loop():
         if active_keys[pygame.K_s] and player.y + player_speed + 40 < height:
             player.y += player_speed
 
-
+        for enemy in enemies:
+            enemy.move(enemy_speed)
 
         #exit game
         for event in pygame.event.get():
